081abd6b2dbd0946440b0de3d158172a574e34e8
Shuzann Hoh <shuzannh@student.unimelb.edu.au>
2019-08-26 14:23:13 +1000

    initial react components

--- artifact-repo/src/App.js
+++ artifact-repo/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/home.component";
+import Upload from "./components/upload.component";
+import MyProfile from "./components/profile.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Artifacts Register</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/upload" className="nav-link">Upload</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/profile" className="nav-link">My Profile</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Route path="/" exact component={Home} />
+          <Route path="/upload" component={Upload} />
+          <Route path="/profile" component={MyProfile} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ artifact-repo/src/components/profile.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MyProfile extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to your profile</p>
+            </div>
+        )
+    }
+}
