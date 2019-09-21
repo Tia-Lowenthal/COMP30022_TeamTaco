2f70d2582eebab79f0df40a95d86a06c53b0ac5e
Tia Lowenthal <tia136@gmail.com>
2019-09-21 18:00:24 +1000

    fixed the delete request

--- artifact-repo/backend/routes/items.js
+++ artifact-repo/backend/routes/items.js
@@ -33,20 +33,20 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').get((req, res) => {
-    Item.findById(req.params.id)
+router.route('/:itemId').get((req, res) => {
+    Item.find({"itemId":req.params.itemId})
         .then(items => res.json(items))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').delete((req, res) => {
-    Item.findByIdAndDelete(req.params.itemId)
+router.route('/:itemId').delete((req, res) => {
+    Item.findOneAndDelete({"itemId":req.params.itemId})
         .then(() => res.json('Item deleted!'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/update/:id').post((req, res) => {
-    Item.findById(req.params.itemId)
+router.route('/update/:itemId').post((req, res) => {
+    Item.findOneAndUpdate({"itemId":req.params.itemId})
         .then(items => {
             item.itemId = req.body.itemId;
             item.userId = req.body.userId;
