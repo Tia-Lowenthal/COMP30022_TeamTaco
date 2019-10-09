094b034dbb12cf9c5303fbf81646d82e53c03124
kaiareyes <karina.andrea.m.reyes@gmail.com>
2019-10-09 16:31:32 +1100

    user login and logout

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
-
+let UserSession = require('../models/usersession.model');
 
 router.route('/').get((req, res) => {
     User.find()
@@ -30,6 +30,20 @@ router.route('/signup').post((req, res) => {
           success: false,
           message: 'Error: Password cannot be blank.'
         });
+    }
+
+    if (!firstname) {
+      return res.send({
+        success: false,
+        message: 'Error: First Name cannot be blank.'
+      });
+    }
+
+    if (!lastname) {
+      return res.send({
+        success: false,
+        message: 'Error: Last Name cannot be blank.'
+      });
     }
 
     User.find({
@@ -73,6 +87,70 @@ router.route('/signup').post((req, res) => {
     });
 });
 
+
+router.route('/login').post((req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  
+
+  if (!email) {
+    return res.send({
+      success: false,
+      message: 'Error: Email cannot be blank.'
+    });
+  }
+
+  if (!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Password cannot be blank.'
+    });
+  }
+
+  User.find({
+    email: email
+  }, (err, users) => {
+    if (err) {
+      console.log('err 2:', err);
+      return res.send({
+        success: false,
+        message: 'Error: server error'
+      });
+    }
+    if (users.length != 1) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    }
+
+    const user = users[0];
+    if (!user.validPassword(password)) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    }
+
+    // Otherwise correct user
+    const userSession = new UserSession();
+    userSession.userId = user._id;
+    userSession.save((err, doc) => {
+      if (err) {
+        console.log(err);
+        return res.send({
+          success: false,
+          message: 'Error: server error'
+        });
+      }
+      return res.send({
+        success: true,
+        message: 'Valid sign in',
+        token: doc._id
+      });
+    });
+  });
+});
 
 router.route('/:userId').get((req, res) => {
     User.find({"userId":req.params.userId})
