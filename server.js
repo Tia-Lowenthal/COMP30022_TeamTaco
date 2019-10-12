89f3c73d2a6df02cda55957a67396a277545f044
kaiareyes <karina.andrea.m.reyes@gmail.com>
2019-10-12 16:08:29 +1100

    removed unnecessary comments

--- server.js
+++ server.js
@@ -49,14 +49,6 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-//if(process.env.NODE_ENV === 'production') {
-//    /*Adds the react production build to serve react requests*/
-//    app.use(express.static(path.join(__dirname, '../client/build')));
-    /*React root*/
-//    app.get('*', (req, res) => {
- //       res.sendFile(path.join(__dirname, '../client/build/index.html'));
- //   });
-//}
 
 const port = process.env.PORT || 5000;
 
