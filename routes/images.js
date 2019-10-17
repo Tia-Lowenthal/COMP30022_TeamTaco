65f9948278da2c883e15e469caa5dd0e9fcc4fcf
Tia-Lowenthal <tia136@gmail.com>
2019-10-17 18:47:22 +1100

    image is uploaded to s3

--- routes/images.js
+++ routes/images.js
@@ -1,34 +1,9 @@
 const router = require('express').Router();
 let Images = require('../models/images.model');
-const multer = require('multer');
-const fs = require('fs');
-
-const storage = multer.diskStorage({
-  destination: function(req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function(req, file, callback) {
-    callback(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
-  }
-});
-
-const fileFilter = (req, file, callback) => {
-  // only accept files of type jpeg, jpg, png, pdf and html
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png' || file.mimetype == 'application/pdf' || file.mimetype == 'text/html') {
-    callback(null, true);
-  } else {
-    callback(new Error('incorrect file type'), false);
-  }
-};
+const upload = require('./config/imageConfig.js');
 
 
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 4// only accept images up to size 4MB
-  },
-  fileFilter: fileFilter
-});
+const singleUpload = upload.single('images');
 
 
 router.route('/').get((req, res) => {
@@ -45,16 +20,9 @@ router.route('/:itemId').get((req, res) => {
 });
 
 
-router.route('/add').post(upload.single('images'), (req, res) => {
-  var newImages = new Images();
-  newImages.images.data = fs.readFileSync(req.file.path);
-  newImages.itemId = req.body.itemId;
-  newImages.images.contentType = req.file.mimetype;
-
-
-    newImages.save()
-        .then(() => res.json('Image added!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.route('/add').post(singleUpload, (req, res) => {
+  console.log(req.file);
+  res.send({'imageUrl': req.file.location});
 });
 
 
