ffb4ff7fcaece3a91b44772f247aaaee337c2446
kaiareyes <karina.andrea.m.reyes@gmail.com>
2019-10-20 11:12:08 +1100

    placeholder for no images

--- client/src/components/itemimages.component.js
+++ client/src/components/itemimages.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import image1 from "./Assets/image1.jpg"
+import placeholder from "./Assets/placeholder.jpg"
 import image2 from "./Assets/image2.jpg"
 import image3 from "./Assets/image3.jpg"
 
@@ -39,37 +40,44 @@ export default class ItemImages extends Component {
             <div>
 
             {!this.state.isLoading ? (
-            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-                {this.state.fetchedItem.images.length>1 && <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>}
-                {this.state.fetchedItem.images.length>=2 && <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>}
-                {this.state.fetchedItem.images.length===3 && <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>}
-            </ol>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                <img className="d-block w-100" src={this.state.fetchedItem.images[0]} alt="First slide"/>
+            <div>
+            {this.state.fetchedItem.images.length>0 ? (
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    {this.state.fetchedItem.images.length>1 && <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>}
+                    {this.state.fetchedItem.images.length>=2 && <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>}
+                    {this.state.fetchedItem.images.length===3 && <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>}
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img className="d-block w-100" src={this.state.fetchedItem.images[0]} alt="First slide"/>
+                    </div>
+                    {this.state.fetchedItem.images.length>=2 && <div className="carousel-item">
+                    <img className="d-block w-100" src={this.state.fetchedItem.images[1]} alt="Second slide"/>
+                    </div>}
+                    {this.state.fetchedItem.images.length===3 && <div className="carousel-item">
+                    <img className="d-block w-100" src={this.state.fetchedItem.images[2]} alt="Second slide"/>
+                    </div>}
                 </div>
-                {this.state.fetchedItem.images.length>=2 && <div className="carousel-item">
-                <img className="d-block w-100" src={this.state.fetchedItem.images[1]} alt="Second slide"/>
-                </div>}
-                {this.state.fetchedItem.images.length===3 && <div className="carousel-item">
-                <img className="d-block w-100" src={this.state.fetchedItem.images[2]} alt="Second slide"/>
-                </div>}
-            </div>
-            {this.state.fetchedItem.images.length > 1 ? (<div> <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
-            </div>) :(null)}
+                {this.state.fetchedItem.images.length > 1 ? (<div> <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+                </div>) :(null)}
+                </div>
+            ) : (
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img className="d-block w-100" src={placeholder} alt="First slide"/>
+                    </div>
+                </div>
+            )}
             </div>
             ) : (null)}
-            
-
-
             </div>
         )
     }
