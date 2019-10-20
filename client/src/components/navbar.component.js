6e9fe68ddfc3b89e030385ac5c83c1f16b11e13c
kaiareyes <karina.andrea.m.reyes@gmail.com>
2019-10-20 20:48:17 +1100

    navbar looks good

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Account from "./accounts.component";
 import '../loginpage.css';
+import './navbar.css';
+import logo from "./Assets/logo2.png"
 
 export default class Navbar extends Component {
     triggerLogout(){
@@ -11,20 +13,20 @@ export default class Navbar extends Component {
     render() {
         
         return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <Link to="/home" className="navbar-brand">Artifacts Register</Link>
+            <div class="whole-nav">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <Link to="/home" className="navbar-brand navbar-logo"><img src = {logo}></img></Link>
                     <div className="collpase navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
-                        <Link to="/home" className="nav-link">Home</Link>
+                        <Link to="/home" className="nav-text">Home</Link>
                         </li>
                         <li className="navbar-item">
-                        <Link to="/upload" className="nav-link">Upload</Link>
+                        <Link to="/upload" className="nav-text">Upload</Link>
                         </li>
                         <li className="navbar-item">
                         <div class="hide-this"><Account ref={account => this.account = account} /></div>
-                        <Link to="" onClick={this.triggerLogout.bind(this)}className="nav-link">Logout</Link>
+                        <Link to="" onClick={this.triggerLogout.bind(this)}className="nav-text">Logout</Link>
                         </li>
                     </ul>
                     </div>
