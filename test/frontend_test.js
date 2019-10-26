7d41de4cc83b370f61a83001790c5e856e987ee4
Julia Zhang <juliaz1@student.unimelb.edu.au>
2019-10-26 19:39:23 +1100

    added tests for login item upload

---
+++ test/frontend_test.js
@@ -0,0 +1,62 @@
+const assert = require('assert');
+var chai = require('chai')
+var chaiHttp = require('chai-http');
+supertest = require('supertest')
+let should = chai.should();
+chai.use(chaiHttp);
+const invalidUserCredentials = {
+    "email": 'invalidemail@gmail.com', 
+    "password": 'invalidpassword'
+  }
+  const validUserCredentials = {
+    "email": 'juliazhang421@gmail.com', 
+    "password": 'juliazhang'
+  }
+
+
+describe( "signin page invalid", function() {
+    it("test login with invalid account", () => {
+        chai.request('https://radiant-dusk-39702.herokuapp.com')
+            .post('/')
+            .send(invalidUserCredentials)
+            .end(function (err, res) {
+                expect((res).to.not.redirect);
+                expect((req).to.be.json);
+             });
+    })
+
+})
+
+describe( "signin page valid", function() {
+    it("test login with valid account", () => {
+        chai.request('https://radiant-dusk-39702.herokuapp.com')
+            .post('/')
+            .send(validUserCredentials)
+            .end(function (err, res) {
+                expect((res.statusCode).to.equal(200));
+                expect((res).to.redirectTo('https://radiant-dusk-39702.herokuapp.com/home'));
+             });
+    })
+
+})
+
+describe("item_page", function(){
+    it("item page is correctly displaying", () => {
+        chai.request('https://radiant-dusk-39702.herokuapp.com')
+            .get('/item/20285263')
+            .end(function(err, res) {
+                expect((res.statusCode).to.equal(200));
+                expect(res.data.should.be.an('array'));
+            });
+    })
+})
+
+describe("upload_page", function(){
+    it("should display with no error", () => {
+        chai.request('https://radiant-dusk-39702.herokuapp.com')
+            .get('/upload')
+            .end(function(err, res) {
+                expect((res.statusCode).to.equal(200));
+            });
+    })
+})
